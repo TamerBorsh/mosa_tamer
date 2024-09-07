b88609f0e467ecaafe33e6890fd13a6b8dc9a4d5
Tamer <tamer1.dev@gmail.com>
2024-09-07 15:03:12 +0300

    5

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Contracts\Permission;
 
 class DatabaseSeeder extends Seeder
 {
@@ -21,7 +22,8 @@ class DatabaseSeeder extends Seeder
         $this->call([
             AdminSeeder::class,
             StateSeeder::class,
-            RegionSeeder::class
+            RegionSeeder::class,
+            PermissionSeeder::class
         ]);
     }
 }
