c9242870cb7e7620ece29806698fbcbd4ffda562
Tamer <tamer1.dev@gmail.com>
2024-09-07 15:49:48 +0300

    5

--- database/seeders/AdminSeeder.php
+++ database/seeders/AdminSeeder.php
@@ -1,9 +1,12 @@
 <?php
 
-use Spatie\Permission\Models\Role;
-use Spatie\Permission\Models\Permission;
+namespace Database\Seeders;
+
 use App\Models\Admin;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
 
 class AdminSeeder extends Seeder
 {
