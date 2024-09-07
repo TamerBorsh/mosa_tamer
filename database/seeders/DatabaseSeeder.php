3b8096ae16b542921a91d4e0ddc2632a2f795658
Tamer <tamer1.dev@gmail.com>
2024-09-07 15:47:17 +0300

    5

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,8 +3,9 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use AdminSeeder;
 use Illuminate\Database\Seeder;
-use Spatie\Permission\Contracts\Permission;
 
 class DatabaseSeeder extends Seeder
 {
