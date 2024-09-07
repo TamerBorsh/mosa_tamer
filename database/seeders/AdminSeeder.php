7a8287ed0f507b0761a40bd15a0861e2545e59eb
Tamer <tamer1.dev@gmail.com>
2024-09-07 15:13:07 +0300

    5

--- database/seeders/AdminSeeder.php
+++ database/seeders/AdminSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use App\Models\Admin;
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
 use Spatie\Permission\Models\Role;
 
 class AdminSeeder extends Seeder
@@ -13,13 +14,26 @@ class AdminSeeder extends Seeder
      */
     public function run(): void
     {
-        Admin::create([
-            'name'      => 'Tamer Alborsh',
-            'username'  =>'tamer',
-            'phone'     =>'0567762233',
-            'password'  =>'password'
-        ]);
+        // Admin::create([
+        //     'name'      => 'Tamer Alborsh',
+        //     'username'  =>'tamer',
+        //     'phone'     =>'0567762233',
+        //     'password'  =>'password'
+        // ]);
         // $user = Admin::first();
         // $user->assignRole(Role::findById(1, 'admin'));
+        $user = Admin::create([
+            'name'      => 'Tamer Alborsh',
+            'username'  => 'tamer',
+            'phone'     => '0567762233',
+            'password'  => "tamer@0599", // تأكد من استخدام bcrypt لتشفير كلمة المرور
+        ]);
+    
+        // إعطاء المستخدم دور محدد (بإمكانك تخصيصه إذا أردت)
+        $user->assignRole(Role::findById(1, 'admin'));
+    
+        // إعطاء جميع الصلاحيات للمستخدم
+        $permissions = Permission::all(); // جلب جميع الصلاحيات
+        $user->givePermissionTo($permissions);
     }
 }
